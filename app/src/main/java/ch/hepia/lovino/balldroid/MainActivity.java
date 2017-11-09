77bff513b13a06a4df6d39a08b71283517d24671
Lovino Maxime <maximelovino@gmail.com>
2017-11-09 22:53:03 +0100

    Choosing a difficulty level is done, created all activities, wired homepage buttons

--- app/src/main/java/ch/hepia/lovino/balldroid/MainActivity.java
+++ app/src/main/java/ch/hepia/lovino/balldroid/MainActivity.java
@@ -1,14 +1,140 @@
 package ch.hepia.lovino.balldroid;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import ch.hepia.lovino.balldroid.models.DifficultyLevels;
 
 public class MainActivity extends Activity {
+    private Button gameButton;
+    private Button highScoreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        highScoreButton = (Button) findViewById(R.id.high_score_button);
+        gameButton = (Button) findViewById(R.id.start_button);
+
+        highScoreButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getBaseContext(), HighScoreActivity.class);
+            startActivity(intent);
+        });
+
+        gameButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getBaseContext(), GameActivity.class);
+            startActivity(intent);
+        });
+
+    }
+
+    /**
+     * Initialize the contents of the Activity's standard options menu.  You
+     * should place your menu items in to <var>menu</var>.
+     * <p>
+     * <p>This is only called once, the first time the options menu is
+     * displayed.  To update the menu every time it is displayed, see
+     * {@link #onPrepareOptionsMenu}.
+     * <p>
+     * <p>The default implementation populates the menu with standard system
+     * menu items.  These are placed in the {@link Menu#CATEGORY_SYSTEM} group so that
+     * they will be correctly ordered with application-defined menu items.
+     * Deriving classes should always call through to the base implementation.
+     * <p>
+     * <p>You can safely hold on to <var>menu</var> (and any items created
+     * from it), making modifications to it as desired, until the next
+     * time onCreateOptionsMenu() is called.
+     * <p>
+     * <p>When you add items to the menu, you can implement the Activity's
+     * {@link #onOptionsItemSelected} method to handle them there.
+     *
+     * @param menu The options menu in which you place your items.
+     * @return You must return true for the menu to be displayed;
+     * if you return false it will not be shown.
+     * @see #onPrepareOptionsMenu
+     * @see #onOptionsItemSelected
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("GAME", MODE_PRIVATE);
+        String diff = prefs.getString("DIFFICULTY", null);
+        if (diff != null) {
+            Log.v("DIFFICULTY MENU", diff);
+            switch (diff) {
+                case DifficultyLevels.EASY:
+                    menu.findItem(R.id.easy_difficulty_button).setChecked(true);
+                    break;
+                case DifficultyLevels.MEDIUM:
+                    menu.findItem(R.id.medium_difficulty_button).setChecked(true);
+                    break;
+                case DifficultyLevels.HARD:
+                    menu.findItem(R.id.hard_difficulty_button).setChecked(true);
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            menu.findItem(R.id.easy_difficulty_button).setChecked(true);
+            prefs.edit().putString("DIFFICULTY", DifficultyLevels.EASY).apply();
+        }
+        return true;
+    }
+
+    /**
+     * This hook is called whenever an item in your options menu is selected.
+     * The default implementation simply returns false to have the normal
+     * processing happen (calling the item's Runnable or sending a message to
+     * its Handler as appropriate).  You can use this method for any items
+     * for which you would like to do processing without those other
+     * facilities.
+     * <p>
+     * <p>Derived classes should call through to the base class for it to
+     * perform the default menu handling.</p>
+     *
+     * @param item The menu item that was selected.
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     * @see #onCreateOptionsMenu
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String toastText;
+
+        String levelToPut;
+        switch (item.getItemId()) {
+            case R.id.easy_difficulty_button:
+                levelToPut = DifficultyLevels.EASY;
+                toastText = "Easy mode selected";
+                break;
+            case R.id.medium_difficulty_button:
+                levelToPut = DifficultyLevels.MEDIUM;
+                toastText = "Medium mode selected";
+                break;
+            case R.id.hard_difficulty_button:
+                levelToPut = DifficultyLevels.HARD;
+                toastText = "Hard mode selected";
+                break;
+            case R.id.about_button:
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        item.setChecked(true);
+        this.getSharedPreferences("GAME", MODE_PRIVATE).edit().putString("DIFFICULTY", levelToPut).apply();
+
+        Toast.makeText(this, toastText, Toast.LENGTH_LONG).show();
+
+        return true;
     }
 }
---
+++ app/src/main/java/ch/hepia/lovino/balldroid/models/DifficultyLevels.java
@@ -0,0 +1,8 @@
+package ch.hepia.lovino.balldroid.models;
+
+
+public final class DifficultyLevels {
+    public static final String EASY = "EASY";
+    public static final String MEDIUM = "MEDIUM";
+    public static final String HARD = "HARD";
+}
